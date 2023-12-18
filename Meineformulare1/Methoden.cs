1f8bdb28a4341383e9e53445e58b1fbd23ef18d2
Erace17 <73711284+Erace17@users.noreply.github.com>
2023-12-18 19:15:27 +0100

    Intermediate status, drawing of shapes in panel in progress

--- Meineformulare1/Methoden.cs
+++ Meineformulare1/Methoden.cs
@@ -14,6 +14,7 @@ namespace Programmsammlung
     public partial class Methoden : Form
     {
         private Panel drawingPanel;
+        private Bitmap drawingBitmap;
         public Methoden()
         {
             InitializeComponent();
@@ -25,76 +26,151 @@ namespace Programmsammlung
             this.Controls.Add(drawingPanel);
 
             //drawingPanel.Paint += new PaintEventHandler(DrawingPanel_Paint);
+            drawingBitmap = new Bitmap(drawingPanel.Width, drawingPanel.Height);
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
         }
 
-        private void comboBox1_SelectedIndexChanged(string com2value, object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string comItem = comboBox1.SelectedItem.ToString();
+            string com2Item = comboBox2.SelectedItem?.ToString();
+            string comItem = comboBox1.SelectedItem?.ToString();
+
+            /*while (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                comItem = comboBox1.SelectedItem.ToString();
+                com2Item = comboBox2.SelectedItem.ToString();
+            }*/
+
+            if (comItem == null || com2Item == null)
+                return;
+
+            ShapeType selectedShapeType = GetShapeType(com2Item);
 
             switch (comItem)
             {
                 case "Methode mit Rückgabewert":
 
-                    DrawSelectedShape(com2value, drawingPanel.CreateGraphics());
+                    
 
-                break;
+                    break;
+
+                case "Methode ohne Rückgabewert":
+
+                    DrawSelectedShape(selectedShapeType);
+
+                    break;
+
+                case "Methode mit Referenz":
+                    break;
 
             }
+
+            drawingPanel.CreateGraphics().DrawImage(drawingBitmap, Point.Empty);
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string comBox = comboBox2.SelectedItem.ToString();
-            comboBox1_SelectedIndexChanged(comBox, sender, e);
-
+            if (comboBox2.SelectedItem != null)
+            {
+                comboBox1_SelectedIndexChanged(sender, e);
+            }
+            
         }
 
-        private void DrawSelectedShape(string com2Value, Graphics g)
+        private void DrawSelectedShape(ShapeType shapeType)
         {
-            switch (com2Value)
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                switch (shapeType)
+                {
+                    case ShapeType.Rechteck:
+
+
+
+                        break;
+
+                    case ShapeType.Parallelogramm:
+
+
+
+                        break;
+
+                    case ShapeType.Raute:
+
+
+
+                        break;
+
+                    case ShapeType.Trapez:
+
+
+
+                        break;
+
+                    case ShapeType.Dreieck:
+
+                        Point[] dreieckPoints = { new Point(50, 50), new Point(150, 50), new Point(100, 150) };
+                        g.FillPolygon(Brushes.Red, dreieckPoints);
+                        break;
+
+                    case ShapeType.Kreis:
+
+
+
+                        break;
+
+                    default:
+
+
+                        break;
+                }
+            }
+        }
+        private ShapeType GetShapeType(string shapeName)
+        {
+            switch(shapeName)
             {
                 case "Rechteck":
-                    
-
-
+                    return ShapeType.Rechteck;
                     break;
 
                 case "Parallelogramm":
-
-
-
+                    return ShapeType.Parallelogramm;
                     break;
 
                 case "Raute":
-
-
-
+                    return ShapeType.Raute;
                     break;
 
                 case "Trapez":
-
-
-
+                    return ShapeType.Trapez;
                     break;
 
                 case "Dreieck":
-
-                    Point[] dreieckPoints = { new Point(50,50), new Point(150,50), new Point(100,150)};
-                    g.FillPolygon(Brushes.Red, dreieckPoints);
-
+                    return ShapeType.Dreieck;
                     break;
 
                 case "Kreis":
-
-
-
+                    return ShapeType.Kreis;
                     break;
 
-                default: 
-                    
-                    
+                default:
+                    return ShapeType.Default;
                     break;
             }
+        }
+
+        public enum ShapeType
+        {
+            Rechteck,
+            Parallelogramm,
+            Raute,
+            Trapez,
+            Dreieck,
+            Kreis,
+            Default
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
