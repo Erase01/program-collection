692ff60ca95c901ec47b2d283f63bcc6b23145b3
Erace17 <73711284+Erace17@users.noreply.github.com>
2023-12-23 04:03:07 +0100

    random examples for all data types

--- Meineformulare1/Variablen.Designer.cs
+++ Meineformulare1/Variablen.Designer.cs
@@ -59,7 +59,6 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(132, 23);
             textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
@@ -67,7 +66,6 @@
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(132, 23);
             textBox2.TabIndex = 2;
-            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // label1
             // 
--- Meineformulare1/Variablen.cs
+++ Meineformulare1/Variablen.cs
@@ -16,16 +16,22 @@ namespace Programmsammlung
         {
             InitializeComponent();
         }
-        public long LongRandom(long min, long max, Random rand)
-        {
-            byte[] buf = new byte[8];
-            rand.NextBytes(buf);
-            long longRand = BitConverter.ToInt64(buf, 0);
 
-            return (Math.Abs(longRand % (max - min)) + min);
-        }
+        private bool button1Clicked = false;
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            UpdateTextBoxes(listBox1.SelectedItem?.ToString());
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            button1Clicked = true;
+            UpdateTextBoxes(listBox1.SelectedItem?.ToString());
+        }
+
+        private void UpdateTextBoxes(string selectedItem)
         {
             string stext = "Hello World";
             char cinhalt = 'A';
@@ -33,14 +39,7 @@ namespace Programmsammlung
             int fvalue = 0;
 
             Random rnd = new Random();
-            int _int16 = rnd.Next(-32768, 32767);
-            int _int32 = rnd.Next(-2147483648, 2147483647);
-            int _byte = rnd.Next(0, 255);
-            int _sbyte = rnd.Next(-128, 127);
             //int _long = rnd.Next(-9223372036854775808, 9223372036854775807);
-
-
-            string boxItem = listBox1.SelectedItem.ToString();
 
             string[] gvar = {   "int16",
                                 "int32",
@@ -61,13 +60,18 @@ namespace Programmsammlung
                                 "char"
                                         };
 
-            if (gvar.Contains(boxItem))
+            if (gvar.Contains(selectedItem))
             {
-                switch (boxItem)
+                switch (selectedItem)
                 {
                     case "int16":
                         textBox1.Text = Int16.MinValue.ToString();
                         textBox2.Text = Int16.MaxValue.ToString();
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = rnd.Next(Int16.MinValue, Int16.MaxValue).ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -76,6 +80,11 @@ namespace Programmsammlung
                     case "int32":
                         textBox1.Text = Int32.MinValue.ToString();
                         textBox2.Text = Int32.MaxValue.ToString();
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = rnd.Next(Int32.MinValue, Int32.MaxValue).ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -84,6 +93,11 @@ namespace Programmsammlung
                     case "byte":
                         textBox1.Text = byte.MinValue.ToString();
                         textBox2.Text = byte.MaxValue.ToString();
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = rnd.Next(Byte.MinValue, Byte.MaxValue).ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -92,6 +106,11 @@ namespace Programmsammlung
                     case "sbyte":
                         textBox1.Text = sbyte.MinValue.ToString();
                         textBox2.Text = sbyte.MaxValue.ToString();
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = rnd.Next(SByte.MinValue, SByte.MaxValue).ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         textBox4.Visible = false;
                         break;
@@ -99,6 +118,12 @@ namespace Programmsammlung
                     case "long":
                         textBox1.Text = long.MinValue.ToString();
                         textBox2.Text = long.MaxValue.ToString();
+                        long randomLongVal = longRandom(rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomLongVal.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -107,6 +132,12 @@ namespace Programmsammlung
                     case "ulong":
                         textBox1.Text = ulong.MinValue.ToString();
                         textBox2.Text = ulong.MaxValue.ToString();
+                        ulong randomUlongVal = ulongRandom(rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomUlongVal.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -115,20 +146,32 @@ namespace Programmsammlung
                     case "bool":
                         textBox1.Text = fvalue.ToString();
                         textBox2.Text = tvalue.ToString();
+                        if (button1Clicked)
+                        {
+                            bool randomBool = rnd.Next(2) == 0;
+                            textBox3.Text = randomBool.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
                         break;
                 }
             }
-            else if (kvar.Contains(boxItem))
+            else if (kvar.Contains(selectedItem))
             {
 
-                switch (boxItem)
+                switch (selectedItem)
                 {
                     case "double":
                         textBox1.Text = double.MinValue.ToString();
                         textBox2.Text = double.MaxValue.ToString();
+                        double randomDoubleVal = doubleRandom(17976931348623, -17976931348623, rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomDoubleVal.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -137,6 +180,12 @@ namespace Programmsammlung
                     case "float":
                         textBox1.Text = float.MinValue.ToString();
                         textBox2.Text = float.MaxValue.ToString();
+                        float randomFloatVal = floatRandom(34028234663852, -34028234663852, rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomFloatVal.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
@@ -145,21 +194,34 @@ namespace Programmsammlung
                     case "decimal":
                         textBox1.Text = decimal.MinValue.ToString();
                         textBox2.Text = decimal.MaxValue.ToString();
+                        decimal randomDecimalVal = DecimalRandom(-79228162514264, 79228162514264,rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomDecimalVal.ToString();
+                            button1Clicked = false;
+                        }
                         textBox4.Clear();
                         label4.Visible = false;
                         textBox4.Visible = false;
                         break;
                 }
             }
-            else if (zvar.Contains(boxItem))
+            else if (zvar.Contains(selectedItem))
             {
 
-                switch (boxItem)
+                switch (selectedItem)
                 {
                     case "string":
                         label4.Visible = true;
                         textBox4.Visible = true;
                         textBox4.Text = stext;
+                        List<string> myStringList = new List<string> { "Option1", "Option2", "Option3" };
+                        string randomString = RandomString(myStringList, rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomString;
+                            button1Clicked = false;
+                        }
                         textBox1.Clear();
                         textBox2.Clear();
                         break;
@@ -168,6 +230,13 @@ namespace Programmsammlung
                         label4.Visible = true;
                         textBox4.Visible = true;
                         textBox4.Text = cinhalt.ToString();
+                        List<char> myCharList = new List<char> { 'A', 'B', 'C', 'D' };
+                        char randomChar = RandomChar(myCharList, rnd);
+                        if (button1Clicked)
+                        {
+                            textBox3.Text = randomChar.ToString();
+                            button1Clicked = false;
+                        }
                         textBox1.Clear();
                         textBox2.Clear();
                         break;
@@ -175,19 +244,72 @@ namespace Programmsammlung
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        public long longRandom(Random rnd)
         {
+            byte[] buffer = new byte[8];
+            rnd.NextBytes(buffer);
+            long randomLong = BitConverter.ToInt64(buffer, 0);
 
+            return (randomLong);
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        public ulong ulongRandom(Random rnd)
         {
+            ulong randomUlong = ((ulong)rnd.Next() << 32) | (ulong)rnd.Next();
 
+            return randomUlong;
         }
 
-        public void button1_Click(object sender, EventArgs e)
+        public double doubleRandom(double lowerBound, double upperBound ,Random rnd)
         {
-         
+            var rDouble = rnd.NextDouble();
+            var rRangeDouble = rDouble * (upperBound - lowerBound) + lowerBound;
+            return rRangeDouble;
+        }
+    
+        public float floatRandom(float lowerBound, float upperBound ,Random rnd)
+        {
+            var rFloat = rnd.NextDouble();
+            var rRangeFloat = rFloat * (upperBound - lowerBound) + lowerBound;
+            return (float)rRangeFloat;
+        }
+
+        public decimal DecimalRandom(decimal minValue, decimal maxValue, Random rnd)
+        {
+            byte scale = (byte)rnd.Next(29);
+            bool sign = rnd.Next(2) == 1;
+
+            decimal randomDecimal = new decimal(
+                rnd.Next(),
+                rnd.Next(),
+                rnd.Next(),
+                sign,
+                scale
+            );
+
+            return Math.Clamp(randomDecimal, minValue, maxValue);
+        }
+
+        public string RandomString(List<string> stringList, Random rnd)
+        {
+            if (stringList == null || stringList.Count == 0)
+            {
+                return null;
+            }
+
+            int randomIndex = rnd.Next(stringList.Count);
+            return stringList[randomIndex];
+        }
+
+        public char RandomChar(List<char> charList, Random rnd)
+        {
+            if (charList == null || charList.Count == 0)
+            {
+                throw new ArgumentException("Die Liste der Zeichen darf nicht leer sein!");
+            }
+
+            int randomIndex = rnd.Next(charList.Count);
+            return charList[randomIndex];
         }
     }
 }
